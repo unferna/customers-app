2175a22dae7658149dccc9a65f6178eeef1f0495
Fernando Florez <arrobaunferna@gmail.com>
2019-05-15 11:49:12 -0500

    Added redux-form

--- src/components/CustomerEdit.jsx
+++ src/components/CustomerEdit.jsx
@@ -1,11 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { reduxForm, Field } from 'redux-form';
+import { setPropsAsInitial } from '../helpers/setPropsAsInitial';
+
+const isRequired = value => {
+    const res = "Este campo es requerido"; 
+    
+    if(value === undefined) {
+        return res;
+
+    } else if( !value.trim() ) {
+        return res;
+        
+    } else {
+        return false;
+    }
+};
+
+const MyField = ({ input, meta }) => (
+    <div>
+        <input { ...input } />
+        {
+            meta.touched && meta.error && <span>{ meta.error }</span>
+        }
+    </div>
+);
 
 const CustomerEdit = ({ name, dni, age }) => {
     return (
         <div>
             <h2>Edición del Cliente</h2>
-            <h3>{name} / {dni} / {age}</h3>
+            <form action="">
+                <div>
+                    <label htmlFor="dni">DNI: </label>
+                    <Field name="dni" component={MyField} type="text" validate={isRequired} />
+                </div>
+                <div>
+                    <label htmlFor="name">Nombre: </label>
+                    <Field name="name" component="input" type="text" />
+                </div>
+                <div>
+                    <label htmlFor="age">Edad: </label>
+                    <Field name="age" component="input" type="number" />
+                </div>
+            </form>
         </div>
     );
 };
@@ -16,4 +54,6 @@ CustomerEdit.propTypes = {
     age: PropTypes.number,
 };
 
-export default CustomerEdit;
+const CustomerEditForm = reduxForm({ form: 'CustomerEdit' })(CustomerEdit);
+
+export default setPropsAsInitial(CustomerEditForm);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import { reducer as reduxForm } from 'redux-form';
 import { customers } from './customers';
 
 export default combineReducers({
-    customers
+    customers,
+    form: reduxForm
 });
